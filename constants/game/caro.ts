4520cd27163cde49ec8d037197b883a15295079d
thinJuno <thinlam22@gmail.com>
2025-10-24 22:32:26 +0700

    delete game caro (soon lastweek)

--- constants/game/caro.ts
+++
@@ -1,7 +0,0 @@
-export const BOARD_SIZE = 13;
-export const WIN_LEN = 5;
-export const QUIZ_TIME_SEC = 20 as const; // thời gian trả lời câu hỏi (giây)
-
-export type Player = 'X' | 'O';
-export const HUMAN: Player = 'X'; // người chơi là X
-export const BOT: Player = 'O'; // bot là O (đi trước)
